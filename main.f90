68c91dc3a46614b6a33153a201309eb00e1cc6ba
われ <yano79269@gmail.com>
2025-01-31 15:14:08 +0900

    Add files via upload

--- main.f90
+++ main.f90
@@ -22,37 +22,31 @@ contains
         logical, intent(in) :: invert
         complex(8), allocatable :: even(:), odd(:)
         real(8), parameter :: PI = 4 * atan(1.0)
-        complex(8) w, wn
-        real(8) ang
+        complex(8) w, t
         integer(8) i, n
         n = ubound(x, 1)
-        if (n .eq. 1) return
+        if (n <= 1) return
 
-        allocate(even(n/2))
-        allocate(odd(n/2))
+        allocate(even(n / 2))
+        allocate(odd(n / 2))
 
         do i = 1, n / 2
-            even(i) = x(i * 2)
-            odd(i) = x(i * 2 - 1)
+            even(i) = x(2 * i - 1)
+            odd(i) = x(2 * i)
         end do
+
         call fft(even, invert)
         call fft(odd, invert)
 
-        if (invert) then
-            ang = -2.0 * PI / n
-        else
-            ang = 2.0 * PI / n
-        end if
-        w = (1.0, 0.0)
-        wn = cmplx(cos(ang), sin(ang), 8)
         do i = 1, n / 2
-            x(i) = odd(i) + w * even(i)
-            x(i + n/2) = odd(i) - w * even(i)
+            w = exp(sum(pack([2.0], invert, [-2.0])) * pi * (0.0, 1.0) * (i - 1) / n)
+            t = w * odd(i)
+            x(i) = even(i) + t
+            x(i + n/2) = even(i) - t
             if (invert) then
                 x(i) = x(i) / 2
                 x(i + n/2) = x(i + n/2) / 2
             end if
-            w = w * wn
         end do
 
         deallocate(odd)
